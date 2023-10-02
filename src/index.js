fe8883ee1cba9bead0f257b2ba631fe923a50d6c
Pedro Hugo <pedrohugoferreira@gmail.com>
2023-10-02 18:27:43 -0300

    Adding elements of cancel and confirm for reset the game

--- src/index.js
+++ src/index.js
@@ -64,17 +64,39 @@ document.querySelector('.check').addEventListener('click', () => {
 
 // Adding event of reintialization of the game
 
-document.querySelector('.btn.again').addEventListener('click', () => {
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = 20;
-  numberAleatory;
-  document.querySelector('.number').textContent = numberAleatory;
-  document.querySelector('.number').textContent = '?';
-});
+// document.querySelector('.btn.again').addEventListener('click', () => {
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.guess').value = '';
+//   document.querySelector('.message').textContent = 'Start guessing...';
+//   document.querySelector('.score').textContent = 20;
+//   numberAleatory;
+//   document.querySelector('.number').textContent = numberAleatory;
+//   document.querySelector('.number').textContent = '?';
+// });
+
+// Adding event of confirm for resetting or not the game
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn.reset');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
 // Adding event that reset the values of the game
-document.querySelector('.btn.reset').addEventListener('click', () => {
+document.querySelector('.btn.ok').addEventListener('click', () => {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
   document.querySelector('.message').textContent = 'Start guessing...';
@@ -83,4 +105,10 @@ document.querySelector('.btn.reset').addEventListener('click', () => {
   numberAleatory;
   document.querySelector('.number').textContent = numberAleatory;
   document.querySelector('.number').textContent = '?';
+  closeModal();
+});
+
+// Adding event for cancel the function of reset
+document.querySelector('.btn.Cancel').addEventListener('click', () => {
+  closeModal();
 });
